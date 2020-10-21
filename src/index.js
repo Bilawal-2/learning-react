6134dc9a7ca417fb50c2e1e7aef087331d0b5079
Muhammad Bilawal <bilawal225@gmail.com>
2020-10-21 17:55:17 +0500

    In this branch I learned nesting element concept

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-function Greeting(){
-  return ( 
-    <div>
-      <h4>Hello world..!</h4>
-    </div>
+
+// function Greeting(){
+//   return ( 
+//     <div>
+//       <h4>Hello world..!</h4>
+//     </div>
+//   );
+//}
+let h1 = React.createElement('h1',null, 'Hello world..!');
+
+const Greeting = () => {
+  return React.createElement('div',
+    {},
+    h1,
+    h1
   );
 }
-
-// const Greeting = () => {
-//   return React.createElement('div',
-//     {},
-//       React.createElement('h1',{},'Hello world')
-//   );
-// }
 
 
 ReactDom.render(<Greeting/>, document.getElementById('root'))
